d3f50cbb7232f0c7ddb1fc3a03f7d58a488f7487
Mickaël Lherminez <45120729+mickaellherminez@users.noreply.github.com>
2024-10-12 19:23:24 +0200

    First commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // Désactive le rendu côté serveur pour un site statique
-  target: 'static', // Utilisez 'static' pour un site statique
+  ssr: true, // Active le rendu côté serveur
+  target: 'server', // Utilisez 'server' pour SSR
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   components: true,
@@ -15,7 +15,5 @@ export default defineNuxtConfig({
       ],
     },
   },
-  generate: {
-    fallback: '404.html' // Ceci génère un fichier 404.html à la racine
-  },
+  // Supprimez la configuration 'generate' car elle n'est pas nécessaire en mode SSR
 })
