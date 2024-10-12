d2c78a3b7dfe56261f2bc05c47460bfa0bac0e7b
Mickaël Lherminez <45120729+mickaellherminez@users.noreply.github.com>
2024-10-12 19:58:29 +0200

    First commit

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Erreur 404</h1>
+    <p>La page que vous recherchez n'existe pas.</p>
+    <button @click="handleError">Retour à l'accueil</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+const handleError = () => {
+  clearError({ redirect: '/' })
+}
+</script>
