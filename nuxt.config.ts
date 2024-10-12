88c54d1f485b1defe811118eddfc0ae3a9e15671
Mickaël Lherminez <45120729+mickaellherminez@users.noreply.github.com>
2024-10-12 19:03:08 +0200

    First commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 import { defineNuxtConfig } from 'nuxt/config';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true, // Active le rendu côté serveur
+  ssr: false, // Active le rendu côté serveur
   target: 'server', // Utilisez 'server' pour SSR
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   components: true,
+  app: {
+    head: {
+      title: 'Mon Application Nuxt',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
 })
