83913815e93d4ce5e5b9e927fa00d88e5c681117
Mickaël Lherminez <45120729+mickaellherminez@users.noreply.github.com>
2024-10-13 12:51:26 +0200

    First commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,68 @@
-import { defineNuxtConfig } from 'nuxt/config';
+import { defineNuxtConfig } from 'nuxt/config'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // Désactive le rendu côté serveur pour un site statique
-  target: 'static', // Utilisez 'static' pour un site statique
+  ssr: false,
+  target: 'static',
+
+  // ... autres configurations ...
   app: {
     head: {
-      title: 'Examples 404',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      ],
-    },
-  },
-  generate: {
-    fallback: 'error.vue' // Spécifie le fichier de repli pour les routes non trouvées
-  },
-  css: [
-    'vfonts/Lato.css',
-    'vfonts/FiraCode.css'
-  ],
-  build: {
-    transpile: ['naive-ui', 'vueuc', '@css-render/vue3-ssr', '@juggle/resize-observer']
-  },
-  vite: {
-    optimizeDeps: {
-      include: ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
     }
   },
+
+  css: [
+    'vfonts/Lato.css'
+  ],
+
+  modules: [
+    '@nuxtjs/robots',
+  ],
+
+  robots: {
+    UserAgent: '*',
+    Disallow: '/error'
+  },
+
+  build: {
+    transpile:
+      process.env.NODE_ENV === 'production'
+        ? [
+            'naive-ui',
+            'vueuc',
+            '@css-render/vue3-ssr',
+            '@juggle/resize-observer'
+          ]
+        : ['@juggle/resize-observer']
+  },
+
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === 'development'
+          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+          : []
+    }
+  },
+
+  nitro: {
+    compressPublicAssets: true,
+  },
+
+  experimental: {
+    payloadExtraction: false
+  },
+
   router: {
     extendRoutes(routes, resolve) {
       routes.push({
         name: 'custom',
         path: '*',
-        component: resolve(__dirname, 'pages/404.vue')
+        component: resolve(__dirname, 'layouts/error.vue')
       })
     }
-  }
+  },
+
+  compatibilityDate: '2024-10-13'
 })
